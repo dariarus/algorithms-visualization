e6a1b3528c84339c30b309f9fb70fb09505d273a
dariarus <rusanova_dv@mail.ru>
2022-07-16 02:08:42 +0300

    testing: создать тесты для алгоритма fib и утилитарных функций

--- src/components/utils/utils.ts
+++ src/components/utils/utils.ts
@@ -1,13 +1,15 @@
 import {TSymbolArray} from "../../types";
 import {ElementStates} from "../../types/element-states";
 
-export const swap = (arr: TSymbolArray, firstIndex: number, secondIndex: number): void => {
+export const swap = (arr: TSymbolArray, firstIndex: number, secondIndex: number) => {
   if (firstIndex === secondIndex) {
     return;
   }
   let temp = arr[firstIndex];
   arr[firstIndex] = arr[secondIndex];
   arr[secondIndex] = temp;
+
+  return arr;
 }
 
 export const setRenderingTimer = async (ms: number) => {
